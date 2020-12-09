cf894a4dae075b55e492f45f6c353b1f35453832
kangxiaoning <kxnmei@163.com>
2020-12-09 15:26:09 +0800

    add custom controller

---
+++ controller.go
@@ -0,0 +1,295 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"fmt"
+	"time"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/util/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/scheme"
+	typedcorev1 "k8s.io/client-go/kubernetes/typed/core/v1"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/record"
+	"k8s.io/client-go/util/workqueue"
+	"k8s.io/klog/v2"
+
+	v1 "github.com/kangxiaoning/learn-kubernetes-crd/pkg/apis/crd/v1"
+	clientset "github.com/kangxiaoning/learn-kubernetes-crd/pkg/client/clientset/versioned"
+	machinescheme "github.com/kangxiaoning/learn-kubernetes-crd/pkg/client/clientset/versioned/scheme"
+	informers "github.com/kangxiaoning/learn-kubernetes-crd/pkg/client/informers/externalversions/crd/v1"
+	listers "github.com/kangxiaoning/learn-kubernetes-crd/pkg/client/listers/crd/v1"
+)
+
+const controllerAgentName = "sample-controller"
+
+const (
+	// SuccessSynced is used as part of the Event 'reason' when a Machine is synced
+	SuccessSynced = "Synced"
+	// ErrResourceExists is used as part of the Event 'reason' when a Machine fails
+	// to sync due to a Deployment of the same name already existing.
+	ErrResourceExists = "ErrResourceExists"
+
+	// MessageResourceExists is the message used for Events when a resource
+	// fails to sync due to a Deployment already existing
+	MessageResourceExists = "Resource %q already exists and is not managed by Machine"
+	// MessageResourceSynced is the message used for an Event fired when a Machine
+	// is synced successfully
+	MessageResourceSynced = "Machine synced successfully"
+)
+
+// Controller is the controller implementation for Machine resources
+type Controller struct {
+	// kubeclientset is a standard kubernetes clientset
+	kubeclientset kubernetes.Interface
+	// machineclientset is a clientset for our own API group
+	machineclientset clientset.Interface
+
+	machinesLister listers.MachineLister
+	machinesSynced cache.InformerSynced
+
+	// workqueue is a rate limited work queue. This is used to queue work to be
+	// processed instead of performing it as soon as a change happens. This
+	// means we can ensure we only process a fixed amount of resources at a
+	// time, and makes it easy to ensure we are never processing the same item
+	// simultaneously in two different workers.
+	workqueue workqueue.RateLimitingInterface
+	// recorder is an event recorder for recording Event resources to the
+	// Kubernetes API.
+	recorder record.EventRecorder
+}
+
+// NewController returns a new sample controller
+func NewController(
+	kubeclientset kubernetes.Interface,
+	machineclientset clientset.Interface,
+	machineInformer informers.MachineInformer) *Controller {
+
+	// Create event broadcaster
+	// Add sample-controller types to the default Kubernetes Scheme so Events can be
+	// logged for sample-controller types.
+	utilruntime.Must(machinescheme.AddToScheme(scheme.Scheme))
+	klog.V(4).Info("Creating event broadcaster")
+	eventBroadcaster := record.NewBroadcaster()
+	eventBroadcaster.StartLogging(klog.Infof)
+	eventBroadcaster.StartRecordingToSink(&typedcorev1.EventSinkImpl{Interface: kubeclientset.CoreV1().Events("")})
+	recorder := eventBroadcaster.NewRecorder(scheme.Scheme, corev1.EventSource{Component: controllerAgentName})
+
+	controller := &Controller{
+		kubeclientset:    kubeclientset,
+		machineclientset: machineclientset,
+		machinesLister:   machineInformer.Lister(),
+		machinesSynced:   machineInformer.Informer().HasSynced,
+		workqueue:        workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "Machines"),
+		recorder:         recorder,
+	}
+
+	klog.Info("Setting up event handlers")
+
+	// Set up an event handler for when Machine resources change
+	machineInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: controller.enqueueMachine,
+		UpdateFunc: func(old, new interface{}) {
+			newMachine := new.(*v1.Machine)
+			oldMachine := old.(*v1.Machine)
+			if newMachine.ResourceVersion == oldMachine.ResourceVersion {
+				// Periodic resync will send update events for all known Deployments.
+				// Two different versions of the same Deployment will always have different RVs.
+				return
+			}
+			controller.enqueueMachine(new)
+		},
+		DeleteFunc: controller.enqueueMachineForDelete,
+	})
+
+	return controller
+}
+
+// Run will set up the event handlers for types we are interested in, as well
+// as syncing informer caches and starting workers. It will block until stopCh
+// is closed, at which point it will shutdown the workqueue and wait for
+// workers to finish processing their current work items.
+func (c *Controller) Run(threadiness int, stopCh <-chan struct{}) error {
+	defer utilruntime.HandleCrash()
+	defer c.workqueue.ShutDown()
+
+	// Start the informer factories to begin populating the informer caches
+	klog.Info("Starting Machine controller")
+
+	// Wait for the caches to be synced before starting workers
+	klog.Info("Waiting for informer caches to sync")
+	if ok := cache.WaitForCacheSync(stopCh, c.machinesSynced); !ok {
+		return fmt.Errorf("failed to wait for caches to sync")
+	}
+
+	klog.Info("Starting workers")
+	// Launch two workers to process Machine resources
+	for i := 0; i < threadiness; i++ {
+		go wait.Until(c.runWorker, time.Second, stopCh)
+	}
+
+	klog.Info("Started workers")
+	<-stopCh
+	klog.Info("Shutting down workers")
+
+	return nil
+}
+
+// runWorker is a long-running function that will continually call the
+// processNextWorkItem function in order to read and process a message on the
+// workqueue.
+func (c *Controller) runWorker() {
+	for c.processNextWorkItem() {
+	}
+}
+
+// processNextWorkItem will read a single work item off the workqueue and
+// attempt to process it, by calling the syncHandler.
+func (c *Controller) processNextWorkItem() bool {
+	obj, shutdown := c.workqueue.Get()
+
+	klog.Infof("get %#v", obj)
+
+	if shutdown {
+		return false
+	}
+
+	// We wrap this block in a func so we can defer c.workqueue.Done.
+	err := func(obj interface{}) error {
+		// We call Done here so the workqueue knows we have finished
+		// processing this item. We also must remember to call Forget if we
+		// do not want this work item being re-queued. For example, we do
+		// not call Forget if a transient error occurs, instead the item is
+		// put back on the workqueue and attempted again after a back-off
+		// period.
+		defer c.workqueue.Done(obj)
+		var key string
+		var ok bool
+		// We expect strings to come off the workqueue. These are of the
+		// form namespace/name. We do this as the delayed nature of the
+		// workqueue means the items in the informer cache may actually be
+		// more up to date that when the item was initially put onto the
+		// workqueue.
+		if key, ok = obj.(string); !ok {
+			// As the item in the workqueue is actually invalid, we call
+			// Forget here else we'd go into a loop of attempting to
+			// process a work item that is invalid.
+			c.workqueue.Forget(obj)
+			utilruntime.HandleError(fmt.Errorf("expected string in workqueue but got %#v", obj))
+			return nil
+		}
+		// Run the syncHandler, passing it the namespace/name string of the
+		// Machine resource to be synced.
+		if err := c.syncHandler(key); err != nil {
+			// Put the item back on the workqueue to handle any transient errors.
+			c.workqueue.AddRateLimited(key)
+			return fmt.Errorf("error syncing '%s': %s, requeuing", key, err.Error())
+		}
+		// Finally, if no error occurs we Forget this item so it does not
+		// get queued again until another change happens.
+		c.workqueue.Forget(obj)
+		klog.Infof("Successfully synced '%s'", key)
+		return nil
+	}(obj)
+
+	if err != nil {
+		klog.Infof("err %s", err)
+		utilruntime.HandleError(err)
+		return true
+	}
+
+	return true
+}
+
+// syncHandler compares the actual state with the desired, and attempts to
+// converge the two. It then updates the Status block of the Machine resource
+// with the current status of the resource.
+func (c *Controller) syncHandler(key string) error {
+	// Convert the namespace/name string into a distinct namespace and name
+	namespace, name, err := cache.SplitMetaNamespaceKey(key)
+	if err != nil {
+		runtime.HandleError(fmt.Errorf("invalid resource key: %s", key))
+		return nil
+	}
+
+	// Get the Machine resource with this namespace/name
+	machine, err := c.machinesLister.Machines(namespace).Get(name)
+	if err != nil {
+		// The Machine resource may no longer exist, in which case we stop
+		// processing.
+		if errors.IsNotFound(err) {
+			klog.Warningf("Machine: %s/%s does not exist in local cache, will delete it from Alpha ...",
+				namespace, name)
+
+			klog.Infof("[Alpha] Deleting machine: %s/%s ...", namespace, name)
+
+			// FIX ME: call Alpha API to delete this machine by name.
+			//
+			// neutron.Delete(namespace, name)
+
+			return nil
+		}
+
+		runtime.HandleError(fmt.Errorf("failed to list machine by: %s/%s", namespace, name))
+
+		return err
+	}
+
+	klog.Infof("[Alpha] Try to process machine: %#v ...", machine)
+
+	// FIX ME: Do diff().
+	//
+	// actualNetwork, exists := neutron.Get(namespace, name)
+	//
+	// if !exists {
+	// 	neutron.Create(namespace, name)
+	// } else if !reflect.DeepEqual(actualNetwork, machine) {
+	// 	neutron.Update(namespace, name)
+	// }
+
+	c.recorder.Event(machine, corev1.EventTypeNormal, SuccessSynced, MessageResourceSynced)
+	return nil
+}
+
+// enqueueMachine takes a Machine resource and converts it into a namespace/name
+// string which is then put onto the work queue. This method should *not* be
+// passed resources of any type other than Machine.
+func (c *Controller) enqueueMachine(obj interface{}) {
+	var key string
+	var err error
+	if key, err = cache.MetaNamespaceKeyFunc(obj); err != nil {
+		runtime.HandleError(err)
+		return
+	}
+	c.workqueue.AddRateLimited(key)
+}
+
+// enqueueMachineForDelete takes a deleted Machine resource and converts it into a namespace/name
+// string which is then put onto the work queue. This method should *not* be
+// passed resources of any type other than Machine.
+func (c *Controller) enqueueMachineForDelete(obj interface{}) {
+	var key string
+	var err error
+	key, err = cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
+	if err != nil {
+		runtime.HandleError(err)
+		return
+	}
+	c.workqueue.AddRateLimited(key)
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,12 @@ module github.com/kangxiaoning/learn-kubernetes-crd
 go 1.15
 
 require (
-	k8s.io/apimachinery v0.19.4
+	github.com/imdario/mergo v0.3.11 // indirect
+	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
+	k8s.io/api v0.19.0
+	k8s.io/apimachinery v0.20.0
+	k8s.io/client-go v0.19.0
 	k8s.io/code-generator v0.19.4
+	k8s.io/klog/v2 v2.4.0
+	k8s.io/utils v0.0.0-20201110183641-67b214c5f920 // indirect
 )
